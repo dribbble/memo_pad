acad80e3d5e211988119da216b8e4ea0fe354645
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2024-01-22 08:54:43 -0600

    bundle gem memo_pad

---
+++ lib/memo_pad.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "memo_pad/version"
+
+module MemoPad
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/memo_pad/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MemoPad
+  VERSION = "0.1.0"
+end
